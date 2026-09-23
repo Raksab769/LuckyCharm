5d1632c585ae86d994fbee59ac70ef75d77fe821
Baskar <baskar3797@gmail.com>
2026-09-23 14:11:58 +0530

    Adjust string stroke width to 2.0dp (v1.9)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.example.luckycharm"
         minSdk = 26
         targetSdk = 34
-        versionCode = 9
-        versionName = "1.8"
+        versionCode = 10
+        versionName = "1.9"
     }
 
     buildTypes {
