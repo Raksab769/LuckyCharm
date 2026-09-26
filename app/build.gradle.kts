b1ffae02ce37486b98f54176ebadaf509b4b7cf9
Baskar <baskar3797@gmail.com>
2026-09-26 13:44:43 +0530

    Push live release v2.6 with removed live preview and realistic butterfly wing animation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.example.luckycharm"
         minSdk = 26
         targetSdk = 34
-        versionCode = 16
-        versionName = "2.5"
+        versionCode = 17
+        versionName = "2.6"
     }
 
     buildTypes {
